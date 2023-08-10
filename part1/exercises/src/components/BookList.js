0efd46a707af4e8ecbc7917263027b860785d2c2
Rina Shovik <rinashovik@gmail.com>
2023-08-10 16:34:08 -0500

    commiting React-1 exercises

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
    let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let book1 = "https://img.thriftbooks.com/api/images/i/m/AC9018A0B59DCFC30E9520F286A6BED6380D0B9D.jpg";
+   let book2 = "https://books.google.com/books/content?id=jeTSu8qm4LUC&printsec=frontcover&img=1&zoom=1&h=160&stbn=1";
+   let book3 = "https://books.google.com/books/content?id=b_3gHAAACAAJ&printsec=frontcover&img=1&zoom=1&h=160&stbn=1";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Mrs Obama" />
+         <img src={book2} alt="History Book!" />
+         <img src={book3} alt="Children Book" />
       </div>      
    );
 }
---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,12 @@
+
+
+
+export default function HobbyLinks(){
+let hobbyLinks = ["https://www.marthastewart.com/1504574/gardening","https://www.tcsworldtravel.com"];
+
+
+return(
+<a hrf={hobbyLinks[0]}>Relaxation</a>
+
+)
+}
